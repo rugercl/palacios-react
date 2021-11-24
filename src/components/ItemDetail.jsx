39762d66f4e254cffd78ad98f6baea798647c15e
Edgardo Palacios <rugercl@gmail.com>
2021-11-24 11:43:14 -0300

    cambiando detalles

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,6 +2,7 @@ import "./item.css";
 import ItemCount from "./ItemCount";
 import {useState} from 'react'
 import {useCartContext} from './CartContext'
+import {Link} from 'react-router-dom'
 
 const ItemDetail = ({producto}) => {
     const [count, setCount] = useState(0)
@@ -18,15 +19,25 @@ const ItemDetail = ({producto}) => {
     return(
         
         <>
-          <div className="card" >
-            <div className="card-header">
-                <h3>{producto.title}</h3>
-            </div><div className="card-body">                
-                <img className="te-imag" src={producto.pictureUrl} alt='' />
-                <p>$ {producto.price}</p>
-            </div>            
-            <ItemCount initial={1} stock={5} onAdd={onAdd} />
-        </div></>
+            <div className="card" >
+                <div className="card-body">
+                <h2>{producto.title}</h2>                
+                    <img className="te-imag" src={producto.pictureUrl} alt='' />
+                </div>  
+            </div>
+            <div className="card-detail">
+                <div className="card-header2">
+                    <h3>Detalles del Producto</h3>
+                    <h4>{producto.description}</h4>
+                    <ItemCount initial={1} stock={5} onAdd={onAdd} />
+                    <Link to='/categoria/todos'>
+                        <button className="btn btn-outline-secondary btn-block">Volver</button>
+                    </Link>
+                    <div className="precio">Precio : $ {producto.price}</div>
+                    <div className="stock">Stock : {producto.stock}</div>
+                </div>
+            </div>
+        </>
             
     )
 
